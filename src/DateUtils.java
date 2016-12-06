d4eae8546a50c37589f69756bcb81291d87aa382
Allan Sumatra <asumatra@vonage.com>
2016-12-06 05:26:27 -0500

    DateUtility

---
+++ src/DateUtils.java
@@ -0,0 +1,19 @@
+
+import java.util.Date;
+
+
+public class DateUtils {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args){
+	
+	
+		System.out.println(getLocalCurrentDate());
+		
+	}
+	private static Date getLocalCurrentDate(){
+		return new Date();
+	}
+}
